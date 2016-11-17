3247d0a6e587b504af2f84bdc2cec06e71ffb61c
Ludovic Rousseau <ludovic.rousseau@free.fr>
2016-11-17 09:15:57 +0100

    PKCS11dotNetV2 autogensh: also try glibtoolize
    On macOS homebew installs glibtoolize instead of libtoolize.
    So if libtoolize is not found we also try glibtoolize.

--- SmartCardServices/src/PKCS11dotNetV2/autogen.sh
+++ SmartCardServices/src/PKCS11dotNetV2/autogen.sh
@@ -7,7 +7,7 @@ set -x
 rm -f configure Makefile.in Makefile config.h.in
 
 aclocal -I .
-libtoolize --force --copy
+libtoolize --force --copy || glibtoolize --force --copy
 
 # we do not use AC_CONFIG_HEADERS
 #autoheader
